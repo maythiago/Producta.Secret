35a392bc6411c3b486c34c77c734aecbcbc48ff2
Thiago May <thiago.may@hbsis.com.br>
2019-04-11 21:13:20 -0300

    Add project files.

---
+++ Producta.Secret/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Producta.Secret
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
